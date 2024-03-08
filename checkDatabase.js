45f7d692bb03239d074c5bf9b6ff724c73b3d849
Nathaniel Blakely <114946291+BiggScrappy@users.noreply.github.com>
2024-03-08 14:31:06 -0500

    Add files via upload

---
+++ checkDatabase.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2');
+
+// MySQL database connection parameters
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "damproject.cp0sgqaywkci.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  password: "adminPass",
+  database: "dam_database"
+});
+
+// Fetch survey data from the database
+pool.query('SELECT * FROM surveys', (err, results) => {
+  if (err) {
+    console.error('Error fetching survey data from the database:', err);
+  } else {
+    console.log('Survey data fetched successfully:', results);
+  }
+
+  // Close the database connection
+  pool.end();
+});
