1b05ef3a6f2b041904d9380f6e4f5cb8d2eedf61
Photini <117546305+Photini-em@users.noreply.github.com>
2024-03-27 12:56:21 -0400

    Update logout.php

--- loginPageANDuserMaker/logout.php
+++ loginPageANDuserMaker/logout.php
@@ -1,9 +1,7 @@
- <!--logout function -->
- <!--Ember Adkins 901893134-->
 <?php
-
 session_start();
 session_destroy();
-
 header("Location: index.php");
 exit;
+ <!--logout function -->
+ <!--Ember Adkins 901893134-->
