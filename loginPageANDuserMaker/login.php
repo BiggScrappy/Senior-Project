52c21bc4ddce7fe0b4f11d679dbc1905c865adac
Photini <117546305+Photini-em@users.noreply.github.com>
2024-04-02 16:57:41 -0400

    Update login.php
    header

--- loginPageANDuserMaker/login.php
+++ loginPageANDuserMaker/login.php
@@ -1,38 +1,6 @@
+
  <!--login-->
  <!--Ember Adkins 901893134-->
-<?php
-
-$is_invalid = false;
-
-if ($_SERVER["REQUEST_METHOD"]==="POST"){
-    $mysqli = require __DIR__ . "/database.php";
-
-    $sql = sprintf("SELECT * FROM users
-            WHERE email = '%s' ",
-            $mysqli->real_escape_string($_POST["email"]) );
-    $result = $mysqli->query($sql);
-
-    $user = $result->fetch_assoc();
-
-    if($user){
-        if(password_verify($_POST["password"], $user["password"])){
-
-            session_start();
-
-            session_regenerate_id();
-
-            $_SESSION["user_id"] = $user["id"];
-            header("Location: index.php");
-            exit;
-        }
-    }
-    $is_invalid = true;
-}
-
-
-?>
-
-
 
 
 <!DOCTYPE html>
@@ -41,16 +9,27 @@ if ($_SERVER["REQUEST_METHOD"]==="POST"){
     <title>Login</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <script src="https://kit.fontawesome.com/c51fcdbfd4.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
+<div class="header">
+  <a href="#default" class="logo">USACE Dam Safety</a>
+  <div class="header-right">
+    <a class="active" href="index.php">Home</a>
+  
+<?php if(isset($_SESSION["user_id"])): ?>
+    <a href="logout.php">Logout</a>
+<?php elseif(!isset($_SESSION["user_id"])): ?>
+    <a href="login.php">Login</a>
+<?php endif; ?>
+  </div>
+</div>
+
     <h1>Login</h1>
 
-    <?php if ($is_invalid): ?>
-        <em> Invalid Login</em>
-    <?php endif; ?>
-
-    <form method=post>
+    <form action="process-login.php" method="post">
         <label for="email">email</label>
         <input type="email" name="email" id="email"
         value="<?= htmlspecialchars($_POST["email"] ?? "")  ?>">
