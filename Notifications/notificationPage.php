a3f73bcb39c0b53ad512aafc783d97465d52ace2
Jedi2001 <114945697+Jedi2001@users.noreply.github.com>
2024-04-03 13:35:02 -0400

    Update notificationPage.php

--- Notifications/notificationPage.php
+++ Notifications/notificationPage.php
@@ -9,24 +9,23 @@
 </head>
 <body>
     <h2>Send Email to Surveyors</h2>
-           <?php
-                 $mysqli = require __DIR__ . "/database.php";
-            ?>
+    <?php
+    $mysqli = require __DIR__ . "/database.php";
+    ?>
 
     <form id="emailForm" action="submitButton.php" method="post">
         <label for="email">Select Email:</label><br>
-             <input type="email" id="email" name="email" multiple list="users"/>
-
-            <datalist id="users">
-     <?php
-        $sql = "select email from users;";
-        $result = $mysqli->query($sql);
-        foreach($result as $i){
-            echo "<option value=\"".$i['email']."\">".$i['email']."</option>"; 
-        }
-    ?>  
-</datalist>
-            <?php  ?>
+        <input type="email" id="email" name="email[]" multiple list="users"/>
+        <datalist id="users">
+            <?php
+            $sql = "SELECT email FROM users;";
+            $result = $mysqli->query($sql);
+            foreach ($result as $row) {
+                echo "<option value=\"" . $row['email'] . "\">" . $row['email'] . "</option>";
+            }
+            ?>
+        </datalist>
+        <option value="all">Select All</option> <!-- Add Select All option here -->
         </select><br><br>
         <label for="subject">Subject:</label><br>
         <input type="text" id="subject" name="subject"><br><br>
@@ -39,6 +38,14 @@
 
     <script>
         $(document).ready(function() {
+            // Listen for changes in the dropdown menu
+            $("#email").change(function() {
+                // If "Select All" option is selected, select all other options
+                if ($(this).val() === "all") {
+                    $("#email option").not(":first").prop("selected", true);
+                }
+            });
+
             // Prevent the form from submitting
             $("#emailForm").submit(function(event) {
                 event.preventDefault();
