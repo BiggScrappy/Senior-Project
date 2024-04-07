3ac31ed78e82af2f6309ffb9eac9bb9a7b7c7e1a
Ember Adkins <117546305+Photini-em@users.noreply.github.com>
2024-04-07 19:59:36 -0400

    Update surveyorSelectPreviousSurveys.php
    usercard

--- surveyorSelectPreviousSurveys.php
+++ surveyorSelectPreviousSurveys.php
@@ -28,6 +28,7 @@ $userID = $user["user_id"];
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
     <script src="https://kit.fontawesome.com/c51fcdbfd4.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="userInformation.css"> 
     <link rel="stylesheet" href="table.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
@@ -47,14 +48,18 @@ $userID = $user["user_id"];
   </div>
 </div>
 <!--Verify User Info-->
-    <h1>Welcome</h1>
+<h1>Welcome</h1>
+    <div class="userInformation">
     <?php if(isset($user)): ?>
-        <p> Hello <?= htmlspecialchars($user["username"]) ?></p>
+        
+        <p> <b> Hello <?= htmlspecialchars($user["username"]) ?>!</b></p>
         <p> Email: <?= htmlspecialchars($user["email"]) ?></p>
-        <p> ID Number: <?= htmlspecialchars($user["user_id"]) ?></p>    
+        <p> Role: <?= htmlspecialchars($user["role_name"]) ?></p> 
+         
     <?php else: ?>
         <p><a href="login.php">Login</a> </p>
     <?php endif; ?>   
+    </div>
 
 <form action="viewPreviousSurveys.php" method="post">
 
