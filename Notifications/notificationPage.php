e5032f88a78e227e67b5cbb089bc1fcb325fac1a
Jedi2001 <114945697+Jedi2001@users.noreply.github.com>
2024-04-11 11:37:15 -0400

    Update notificationPage.php
    bug fix

--- Notifications/notificationPage.php
+++ Notifications/notificationPage.php
@@ -1,17 +1,16 @@
 <?php
-   ini_set('session.save_path',realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/home1/missysme/sessions'));
-     session_start();
+    ini_set('session.save_path',realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/home1/missysme/sessions'));
+    session_start();
 
-     //verify user info
-     if(isset($_SESSION["user_id"])){
-
+    //verify user info
+    if(isset($_SESSION["user_id"])){
         $mysqli = require __DIR__ . "/database.php";
-    
+
         $sql = "SELECT * FROM User_Information
                 WHERE user_id = {$_SESSION["user_id"]}";
-        
+
         $result = $mysqli->query($sql);
-    
+
         $user = $result->fetch_assoc();
     }
 ?>
@@ -91,8 +90,8 @@
             <option value="">No Survey Selected</option>
             <?php
             // Populate survey select options from database
-            foreach ($surveyOptions as $option) {
-                echo "<option value='" . $option . "'>" . $option . "</option>";
+            foreach ($surveyOptions as $surveyId => $optionText) {
+                echo "<option value='" . $surveyId . "'>" . $optionText . "</option>";
             }
             ?>
         </select><br><br>
